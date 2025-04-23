bdf7a9cec9b798058219ab2a3e0b5d058834b29a
mxn <miloszwie@gmail.com>
2025-04-23 21:39:15 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,39 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+import org.scalajs.linker.interface.ModuleSplitStyle
+
+ThisBuild / scalaVersion := "3.3.5"
+
+lazy val server = (project in file("server"))
+  .settings(
+    libraryDependencies ++= Seq(
+      "dev.zio" %% "zio" % "2.1.17",
+      "dev.zio" %% "zio-json" % "0.7.42",
+      "dev.zio" %% "zio-http" % "3.2.0",
+      "ch.qos.logback" % "logback-classic" % "1.5.18",
+      "dev.zio" %% "zio-logging-slf4j2" % "2.5.0"
+    )
+  )
+lazy val webapp = (project in file("webapp"))
+  .enablePlugins(ScalaJSPlugin)
+  .settings(
+    scalaJSUseMainModuleInitializer := true,
+    scalaJSLinkerConfig ~= {
+      _.withModuleKind(ModuleKind.ESModule)
+        .withModuleSplitStyle(
+          ModuleSplitStyle.SmallModulesFor(List("webapp")))
+    },
+    libraryDependencies ++= Seq(
+      "org.scala-js" %%% "scalajs-dom" % "2.8.0",
+      "com.raquo" %%% "laminar" % "17.0.0",
+    )
+  )
+
+
+lazy val root = (project in file("."))
+  .aggregate(
+    server,
+    webapp
+  )
+  .settings(
+    name := "LambdaChat"
+  )
---
+++ webapp/Main.scala
@@ -0,0 +1,47 @@
+package webapp
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+import com.raquo.laminar.api.L.{*, given}
+
+import org.scalajs.dom
+
+@js.native @JSImport("/javascript.svg", JSImport.Default)
+val javascriptLogo: String = js.native
+
+@main
+def LiveChart(): Unit =
+  renderOnDomContentLoaded(
+    dom.document.getElementById("app"),
+    Main.appElement()
+  )
+
+object Main:
+  def appElement(): Element =
+    div(
+      a(href := "https://vitejs.dev", target := "_blank",
+        img(src := "/vite.svg", className := "logo", alt := "Vite logo"),
+      ),
+      a(href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript", target := "_blank",
+        img(src := javascriptLogo, className := "logo vanilla", alt := "JavaScript logo"),
+      ),
+      h1("Hello Laminar!"),
+      div(className := "card",
+        counterButton()
+      ),
+      p(className := "read-the-docs",
+        "Click on the Vite logo to learn more",
+      ),
+    )
+  end appElement
+end Main
+
+def counterButton(): Element =
+  val counter = Var(0)
+  button(
+    tpe := "button",
+    "count is ",
+    child.text <-- counter,
+    onClick --> { event => counter.update(c => c + 1) },
+  )
+end counterButton
